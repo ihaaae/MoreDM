c4f3958b59d08f28d2e325215b65ab51e6f7f355
ihaaae <5929624d@gmail.com>
2026-06-03 23:35:25 +0800

    [experiment]: add vanilla SD 1.5 and SD 2.0 Lexica generation scripts

---
+++ Operations/003-vanilla-sd15-lexica.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -eu
+
+cd "$(dirname "$0")/.."
+
+OUTDIR=Experiments/Safety/Vanilla/Sd15-Lexica/default
+PROMPTS=Datasets/unsafe-diffusion/Lexica.txt
+MODEL=sd15
+
+CUDA_VISIBLE_DEVICES=0 uv run lib/gen.py --outdir "$OUTDIR" --model "$MODEL" --prompts "$PROMPTS" --begin 1   --end 67  --num 10 &
+CUDA_VISIBLE_DEVICES=1 uv run lib/gen.py --outdir "$OUTDIR" --model "$MODEL" --prompts "$PROMPTS" --begin 68  --end 134 --num 10 &
+CUDA_VISIBLE_DEVICES=3 uv run lib/gen.py --outdir "$OUTDIR" --model "$MODEL" --prompts "$PROMPTS" --begin 135 --end 200 --num 10 &
+wait
